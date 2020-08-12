9d553f2879403511042835caef16fee7716bdbe2
gufeltrin <guhfis@gmail.com>
2020-08-12 09:43:33 -0300

    teste 2

--- sensor_CAN/src/main.cpp
+++ sensor_CAN/src/main.cpp
@@ -1,5 +1,17 @@
 #include <Arduino.h>
 
+//Pinos da CAN - pinagem do modulo mcp2515
+
+#define CAN_SCK 13
+#define CAN_SI 11
+#define CAN_SO 12
+#define CAN_CS 10
+
+//Pino do sensor
+
+#define PIN_SPEED 3
+
+
 void setup() {
   // put your setup code here, to run once:
 }
