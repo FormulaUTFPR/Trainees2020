39a895db2edca397a1095efbb2af5c2a45116b6a
Gabriel Raul <rraulggabriell@gmail.com>
2020-08-18 14:11:04 -0300

    LEDs_cmt_3

--- Fita_de_LEDs/src/main.cpp
+++ Fita_de_LEDs/src/main.cpp
@@ -1,34 +1,49 @@
-#include <Arduino.h>
-#include <mcp2515.h>
+#include <Arduino.h> //Arduino function libery 
+#include <mcp2515.h> //mcp2515 module function libery 
 #include <TimerOne.h> 
-#include <SPI.h> //Biblioteca de funções do protocolo SPI
+#include <SPI.h> //SPI protocol function libery 
 
-//--------------------informações úteis--------------------//
-//PACOTE DE RPM:21:13 06/08/2020
 
-//ID do pacote de RPM: 0x002
+#define LED_RPM 40    // define LED tape as 40 pin
 
-//DLC do pacote: 2
+//mcp2515 module (Receive CAN and send as SPI to Arduino)
+#define SPI_SCK 52    // define SPI_SCK (SPI's clock bus) as 52 pin
+#define SPI_SI 51     // define SPI_SI  (SPI's slave input bus) as 51 pin
+#define SPI_SO 50     // define SPI_SO  (slave output do SPI bus) as 50 pin
+#define SPI_CS_CAN 53 // define SPI_CS_CAN  (CAN's slave select on SPI bus) as 53 pin
 
-//	INFORMAÇÕES DA CAN:Velocidade da rede: 1000KBPS
-//--------------------------------------------------------//
 
-#define LED_RPM 40 //Pino da fita de LED
-
-void setup() {
-
-//PINAGEM 
-
-#define SPI_SCK 52    // define SPI_SCK (clock do SPI bus) no pino 52
-#define SPI_SI 51     // define SPI_SI  (slave input do SPI bus) no pino 51
-#define SPI_SO 50     // define SPI_SO  (slave output do SPI bus) no pino 50
-#define SPI_CS_CAN 53 // define SPI_CS_CAN  (slave select da CAN no SPI bus) no pino 53
-
+struct can_frame canMSG; // define package received fron CAN 
 
 MCP2515 mcp2515(SPI_CS_CAN); // cria uma instância do módulo transciever CAN (configura pino CS)
 
+void setup() {
+
+  Serial.begin(9600);
+  SPI.begin();
+
+  mcp2515.reset();
+  mcp2515.setBitrate(CAN_1000KBPS,MCP_8MHZ); //set CAN speed and clock frequency
+  mcp2515.setNormalMode();
+
+  RPM.can_id = 0x002;
+  RPM.can_dlc = 2;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  If(mcp2515.readMessage(&canMSG)==MCP2515: :ERROR_OK) {  //Check if data received is OK
+
+    if(canMSG.can_id==0x002){
+
+        //ler pacote do RPM, ver se precisa colocar na escala certa, associar escala de RPM com escala de altura e cor na fita de LEDs
+
+      }
+
+
+    }
+  else(){
+    Serial.print("Package error!")
+  }
+
 }
