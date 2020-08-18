1f3e9c8baa568a0c0fa8f7b3b1465719d5902c0e
gufeltrin <guhfis@gmail.com>
2020-08-18 11:01:26 -0300

    vs1

--- sensor_CAN/src/main.cpp
+++ sensor_CAN/src/main.cpp
@@ -1,4 +1,7 @@
 #include <Arduino.h>
+#include <CAN.h>
+#include <mcp2515.h>
+#include <TimerOne.h>
 
 //Pinos da CAN - pinagem do modulo mcp2515
 
@@ -11,9 +14,34 @@
 
 #define PIN_SPEED 3
 
+//id sensor de RPM
+#define ID_ADDRESS_SENSOR_RPM 0x002
+
+//Criar Pacotes para CAN
+can_frame canRPM;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600); //inicia a serial para debug
+  Serial.println("Transmissor CAN");
+
+  setupCAN();
+
+  // Inicia o barramento CAN a 1000 kbps
+  if (!CAN.begin(1E6)) {
+    Serial.println("Falha ao iniciar o controlador CAN"); //caso não seja possível iniciar o controlador
+    while (1);
+  }
+}
+
+void setupCAN() {
+  canRPM.can_id = ID_ADDRESS_SENSOR_RPM;
+  canRPM.can_dlc = 2;
+}
+
+void sensorRPM(void) {
+  float voltage;
+  voltage = map(analogRead(PIN_SPEED));
+  canRPM.data[0] = ();
 }
 
 void loop() {
