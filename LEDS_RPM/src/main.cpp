d12a1ab931cd6be3a8a8e146c41d7caf7e1b70db
WilSouza <willian_souza_cardoso@hotmail.com>
2020-08-19 19:53:15 -0300

    cmt4

--- LEDS_RPM/src/main.cpp
+++ LEDS_RPM/src/main.cpp
@@ -1,9 +1,21 @@
 #include <Arduino.h>
+#include <mcp2515.h>
+#include <TimerOne.h>
+#include <SPI.h>
+
+
+#define SPI_SCK 52 // (relógio do barramento) pino 52
+#define SPI_SI 51 // (entrada escrava barramento) pino 51
+#define SPI_SO 50 // (saída escrava barramento) pino 50
+#define SPI_CS_CAN 53 // (seleção escravo CAN...) pino 53
+#define LED_RPM 40 // (fita de LED) pino 40
+
+MCP2515 mcp2515 (SPI_CS_CAN); // (configura pino CS)cria uma instancia do modulo transcrever CAN 
 
 void setup() {
   // put your setup code here, to run once:
 }
-ARQUIVO TESTE 1
+
 void loop() {
   // put your main code here, to run repeatedly:
 }
