d1f8afd33fe02488e0edae6fd447bbd8ead42e22
Bruno <boschini@alunos.utfpr.edu.br>
2020-08-19 16:10:12 -0300

    Sensor_cmt1

---
+++ RPM_Sensor/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#include <mcp2515.h>
+#include <TimerOne.h>
+
+void setup() 
+{
+  #define CAN_SCK 13
+  #define CAN_SI 11
+  #define CAN_SO 12
+  #define CAN_CS 10
+  #define PIN_SPEED 3
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
